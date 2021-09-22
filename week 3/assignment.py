5a3020ec598378f62e6440b057feba864fc4fcd5
Skuld Norniern <43695854+SkuldNorniern@users.noreply.github.com>
2021-09-22 23:49:18 +0900

    [MODIFY] Merged two files to one
    /week 3
        / assignment.py
        * merged the content of 박지민
        - 박지민

--- week 3/assignment.py
+++ week 3/assignment.py
@@ -1,3 +1,11 @@
+#박윤재 - Skuld Norniern
+
 ln = int(input("write the number: "))
 for i in range(ln):
     print('*' * (i+1), end="\n")
+
+#박지민 - wlals3665 
+n = int(input("숫자를 입력하세요 "))
+
+for i in range(1, n+1):
+    print("*"*i)
